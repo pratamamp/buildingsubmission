730bb79feeeaae2cefa1b75a1efb9112e3edca39
pratama mp <pratamamp@pratamas-MacBook-Pro.local>
2022-09-02 17:08:50 +0700

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <div className="relative">
+      <Routes>
+        <Route path="" />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
