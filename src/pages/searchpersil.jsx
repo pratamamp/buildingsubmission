c6bbfe71ee84f134c7816f4579be8157887ffc6b
pratamamp <pratamamp@gmail.com>
2022-09-07 22:58:59 +0700

    update search location

--- src/pages/searchpersil.jsx
+++ src/pages/searchpersil.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function SearchPersil() {
   const navigate = useNavigate();
-
+  const [select, setSelect] = useState(true);
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -11,28 +11,72 @@ function SearchPersil() {
   };
   return (
     <div className="flex">
-      <div className="w-1/4 bg-gray-100 h-[calc(100vh_-_9.5rem)] p-4 flex flex-col">
-        <h2 className="font-poppins font-semibold">Pilih Persil</h2>
-        <p className=" font-poppins text-xs my-2">
-          Pilih Atau Cari Persil Tanah Untuk Pengajuan Melalui Peta Yang
-          Tersedia
-        </p>
-        <div className="w-full flex flex-grow items-center justify-center px-5 border-b-2">
-          <h2 className="font-poppins text-xs text-[#12519E] text-center">
-            Anda Belum Memilih Lokasi Untuk Pengajuan IMB
+      <div className="w-1/4 bg-gray-100 h-[calc(100vh_-_9.5rem)] px-4 flex flex-col text-[#424242]">
+        <div>
+          <h2 className="font-poppins font-semibold text-black">
+            Pilih Persil
           </h2>
+          <p className=" font-poppins text-xs my-2 text-[#5C5C5C]">
+            Pilih Atau Cari Persil Tanah Untuk Pengajuan Melalui Peta Yang
+            Tersedia
+          </p>
         </div>
-        <div className="flex justify-evenly items-center my-3 font-poppins text-xs">
-          <button className="border-2 rounded-lg px-3 py-2 w-28 text-gray-700">
-            Batal
-          </button>
-          <button
-            className="w-28 px-3 py-2 rounded-lg bg-[#12519E] text-white"
-            onClick={handleSubmit}
-          >
-            Selanjutnya
-          </button>
-        </div>
+        {!select ? (
+          <div className="w-full flex flex-grow items-center justify-center px-5 border-b-2">
+            <h2 className="font-poppins text-xs text-[#12519E] text-center">
+              Anda Belum Memilih Lokasi Untuk Pengajuan IMB
+            </h2>
+          </div>
+        ) : (
+          <>
+            <div className="border-b border-gray-200 font-poppins py-3 space-y-2 overflow-y-auto no-scrollbar">
+              <div className="text-xs">
+                <h2 className="font-semibold text-sm text-[#12519E]">Lokasi</h2>
+                <ul className="space-y-1">
+                  <li>Kota</li>
+                  <li>Kecamatan</li>
+                  <li>kelurahan</li>
+                  <li>RT/RW</li>
+                </ul>
+              </div>
+
+              <div className="text-xs">
+                <h2 className="font-semibold text-sm text-[#12519E]">
+                  Informasi Sub Zona
+                </h2>
+                <ul className="space-y-1">
+                  <li>Zona</li>
+                  <li>Sub-Zona</li>
+                  <li>Luas Bidang</li>
+                  <li>KDB</li>
+                  <li>KLB</li>
+                  <li>KDH</li>
+                  <li>Ketinggian</li>
+                  <li>PSL</li>
+                  <li>Tipe</li>
+                </ul>
+              </div>
+
+              <div className="text-xs space-y-1">
+                <h2 className="font-semibold text-sm text-[#12519E]">
+                  Informasi Lainnya
+                </h2>
+                <p>Metode Penggambaran</p>
+              </div>
+            </div>
+            <div className="flex justify-evenly items-center font-poppins text-xs w-full my-auto">
+              <button className="border rounded-lg px-3 py-2 w-28 my-2 text-gray-700">
+                Batal
+              </button>
+              <button
+                className="w-28 px-3 py-2 rounded-lg bg-[#12519E] text-white my-2"
+                onClick={handleSubmit}
+              >
+                Selanjutnya
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
--- src/pages/upload.jsx
+++ src/pages/upload.jsx
@@ -1,54 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaGlobe, FaPencilAlt } from "react-icons/fa";
 import { BiTargetLock } from "react-icons/bi";
 import { BsFileEarmarkText } from "react-icons/bs";
 import { ImCloudUpload } from "react-icons/im";
 
 function UploadFiles() {
+  const [loading, setLoading] = useState(false);
+
   return (
     <div className="w-full h-[calc(100vh_-_9.5rem)] flex justify-center items-center">
-      <div className="flex justify-around w-11/12 h-5/6 bg-white rounded-lg shadow-lg shadow-gray-300 font-poppins">
-        <div className="m-10 mr-28 w-1/4 font-poppins">
-          <h2 className="font-semibold text-lg">Upload GPA 3D</h2>
-          <p className="text-xs text-[#5C5C5C] my-3">
-            Sebelum mengupload file . harap perhatikanlah ketentuan file dibawah
-            ini
-          </p>
-          <div className="flex flex-col divide-y text-xs mt-10">
-            <div className="flex items-center h-10 space-x-3">
-              <FaGlobe className="w-4 h-4 text-[#12519E]" />
-              <h2 className="text-[#717171]">Sudah dilakukan geo-referensi</h2>
-            </div>
-            <div className="items-center flex h-10 space-x-3">
-              <BiTargetLock className="w-4 h-4 text-[#12519E]" />
-              <h2 className="text-[#717171]">
-                Menggunakan sistem koordinat wgs 1984
-              </h2>
-            </div>
-            <div className="flex items-center h-10 space-x-3">
-              <BsFileEarmarkText className="w-4 h-4 text-[#12519E]" />
-              <h2 className="text-[#717171]">
-                Unggah model 3d GPA berbasis BIM dalam format .SKP atau .RVT
-              </h2>
-            </div>
-            <div className="flex items-center h-10 space-x-3">
-              <FaPencilAlt className="w-4 h-4 text-[#12519E]" />
-              <h2 className="text-[#717171]">Ketentuan lainnya</h2>
+      <div className="justify-around w-11/12 h-5/6 bg-white rounded-lg shadow-lg shadow-gray-300 font-poppins">
+        <div className="flex">
+          <div className="m-10 mr-28 w-1/4 font-poppins">
+            <h2 className="font-semibold text-lg">Upload GPA 3D</h2>
+            <p className="text-xs text-[#5C5C5C] my-3">
+              Sebelum mengupload file . harap perhatikanlah ketentuan file
+              dibawah ini
+            </p>
+            <div className="flex flex-col divide-y text-xs mt-10">
+              <div className="flex items-center h-10 space-x-3">
+                <FaGlobe className="w-4 h-4 text-[#12519E]" />
+                <h2 className="text-[#717171]">
+                  Sudah dilakukan geo-referensi
+                </h2>
+              </div>
+              <div className="items-center flex h-10 space-x-3">
+                <BiTargetLock className="w-4 h-4 text-[#12519E]" />
+                <h2 className="text-[#717171]">
+                  Menggunakan sistem koordinat wgs 1984
+                </h2>
+              </div>
+              <div className="flex items-center h-10 space-x-3">
+                <BsFileEarmarkText className="w-4 h-4 text-[#12519E]" />
+                <h2 className="text-[#717171]">
+                  Unggah model 3d GPA berbasis BIM dalam format .SKP atau .RVT
+                </h2>
+              </div>
+              <div className="flex items-center h-10 space-x-3">
+                <FaPencilAlt className="w-4 h-4 text-[#12519E]" />
+                <h2 className="text-[#717171]">Ketentuan lainnya</h2>
+              </div>
             </div>
           </div>
+
+          <div className=" border rounded-md border-[#0161D5] flex flex-col justify-center bg-[#FAFCFE] w-80 items-center">
+            <ImCloudUpload className="w-20 h-20 mb-5 text-[#0161D5]" />
+            <p className="font-poppins text-xs text-[#37474F]">
+              <span className="font-bold">
+                Taruh file .SKP atau .RVT disini
+              </span>{" "}
+              atau klik untuk menggunggah
+            </p>
+            <p className="font-poppins text-xs text-[#717171]">
+              Maximum file size 100 MB
+            </p>
+            <button className="mt-20 rounded-lg border-[#BDBDBD] border p-2 font-poppins text-xs text-[#12519E] font-semibold">
+              Upload +
+            </button>
+          </div>
         </div>
-        <div className=" border rounded-md border-[#0161D5] flex-auto flex flex-col justify-center bg-[#FAFCFE] m-10 items-center">
-          <ImCloudUpload className="w-20 h-20 mb-5 text-[#0161D5]" />
-          <p className="font-poppins text-xs text-[#37474F]">
-            <span className="font-bold">Taruh file .SKP atau .RVT disini</span>{" "}
-            atau klik untuk menggunggah
-          </p>
-          <p className="font-poppins text-xs text-[#717171]">
-            Maximum file size 100 MB
-          </p>
-          <button className="mt-20 rounded-lg border-[#BDBDBD] border p-2 font-poppins text-xs text-[#12519E] font-semibold">
-            Upload +
-          </button>
+        <div className="flex justify-end mx-10">
+          <button>Selanjutnya</button>
         </div>
       </div>
     </div>
