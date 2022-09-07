c31d9133c65e4ddd0b365dc7053e93835a207646
pratamamp <pratamamp@gmail.com>
2022-09-07 14:37:28 +0700

    search pages

--- src/pages/searchpersil.jsx
+++ src/pages/searchpersil.jsx
@@ -9,10 +9,16 @@ function SearchPersil() {
           Pilih Atau Cari Persil Tanah Untuk Pengajuan Melalui Peta Yang
           Tersedia
         </p>
-        <div className="w-full flex flex-grow items-center justify-center px-5">
+        <div className="w-full flex flex-grow items-center justify-center px-5 border-b-2">
           <h2 className="font-poppins text-xs text-blue-400 text-center">
             Anda Belum Memilih Lokasi Untuk Pengajuan IMB
           </h2>
+        </div>
+        <div className="flex justify-evenly items-center my-3 font-poppins text-xs">
+          <button className="border-2 rounded-lg p-2 w-28">Batal</button>
+          <button className="w-28 p-2 rounded-lg bg-[#12519E] text-white">
+            Selanjutnya
+          </button>
         </div>
       </div>
     </div>
