b6fd7e8ebdb44d94b201c52743e288ec4175c5bf
Vian <pgustandika@esriindonesia.co.id>
2022-09-07 16:19:15 +0700

    pilih persil init

---
+++ src/components/Persil/Persil.jsx
@@ -0,0 +1,88 @@
+import "@arcgis/core/assets/esri/themes/light/main.css";
+import React, { useRef, useEffect } from "react";
+import esriConfig from "@arcgis/core/config";
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import Search from "@arcgis/core/widgets/Search";
+import Editor from "@arcgis/core/widgets/Editor";
+
+export const PersilMap = ({ className, setDataPersil }) => {
+  esriConfig.apiKey =
+    "AAPK4b3895d07794469dbd57a083f1ac4fa52nP3iH2KjKBQ2wRSVFdz17o7vuCF-lo_yMcmcIDhijUKcuRGC_oNSu5JKRBZDjYn";
+
+  const mapRef = useRef(null);
+  useEffect(() => {
+    const persilFeature = new FeatureLayer({
+      url: "https://demo.esriindonesia.co.id/arcgis/rest/services/Hosted/KRK_Persil/FeatureServer/3",
+      // url: "https://tataruang.jakarta.go.id/server/rest/services/peta_operasional/Informasi_Rencana_Kota_DKI_Jakarta_View/FeatureServer/3",
+      outFields: ["*"],
+    });
+
+    const locationFeature = new FeatureLayer({
+      url: "https://demo.esriindonesia.co.id/arcgis/rest/services/Hosted/KoordinatGedung_1/FeatureServer/0",
+    });
+
+    const map = new Map({
+      basemap: "arcgis-topographic", // Basemap layer service
+      layers: [persilFeature, locationFeature],
+    });
+    const view = new MapView({
+      map: map,
+      center: [106.79752143200005, -6.255], // Longitude, latitude
+      zoom: 18, // Zoom level
+      container: mapRef.current,
+    });
+
+    const search = new Search({
+      view: view,
+      placeholder: "Cari Lokasi Persil Tanah...",
+    });
+
+    const editor = new Editor({
+      view: view,
+      allowedWorkflows: "create",
+      snappingOptions: {
+        enabled: false,
+        featureSources: [{ layer: locationFeature }],
+        featureEnabled: false,
+        selfEnabled: false,
+      },
+      layerInfos: [
+        {
+          layer: locationFeature, // pass in the feature layer,
+          enabled: true, // default is true, set to false to disable editing functionality
+          addEnabled: true, // default is true, set to false to disable the ability to add a new feature
+          updateEnabled: true, // default is true, set to false to disable the ability to edit an existing feature
+          deleteEnabled: true, // default is true, set to false to disable the ability to delete features
+        },
+        {
+          layer: persilFeature, // pass in the feature layer,
+          enabled: false,
+        },
+      ],
+    });
+    view.ui.add(search, "top-right");
+    view.ui.add(editor, "bottom-right");
+
+    let selectedHighlight;
+
+    view.on("click", function (event) {
+      view.hitTest(event).then(function (response) {
+        const attributes = response.results[0].graphic.attributes;
+        setDataPersil(attributes);
+        view.whenLayerView(persilFeature).then((layerView) => {
+          if (selectedHighlight) {
+            selectedHighlight.remove();
+          }
+          selectedHighlight = layerView.highlight(attributes?.objectid);
+        });
+      });
+    });
+
+    return () => {
+      view && view.destroy();
+    };
+  }, []);
+  return <div ref={mapRef} className={className} />;
+};
--- src/pages/searchpersil.jsx
+++ src/pages/searchpersil.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { PersilMap } from "../components/Persil/Persil";
 
 function SearchPersil() {
+  const [dataPersil, setDataPersil] = useState(null);
   return (
     <div className="flex">
       <div className="w-1/4 bg-gray-100 h-[calc(100vh_-_9.5rem)] p-4 flex flex-col">
@@ -10,9 +12,43 @@ function SearchPersil() {
           Tersedia
         </p>
         <div className="w-full flex flex-grow items-center justify-center px-5 border-b-2">
-          <h2 className="font-poppins text-xs text-blue-400 text-center">
-            Anda Belum Memilih Lokasi Untuk Pengajuan IMB
-          </h2>
+          {dataPersil ? (
+            <ul>
+              <li>
+                Kota:{" "}
+                {String(dataPersil.nama_kota)
+                  ?.toLowerCase()
+                  .replace(/(^\w|\s\w)/g, (m) => m.toUpperCase())}
+              </li>
+              <li>
+                Kecamatan:{" "}
+                {dataPersil.kecamatan
+                  ?.toLowerCase()
+                  .replace(/(^\w|\s\w)/g, (m) => m.toUpperCase())}
+              </li>
+              <li>
+                Kelurahan:{" "}
+                {dataPersil.kelurahan
+                  ?.toLowerCase()
+                  .replace(/(^\w|\s\w)/g, (m) => m.toUpperCase())}
+              </li>
+              <li>RT/RW: -</li>
+              <li>Zona: {dataPersil.zona}</li>
+              <li>Sub-Zona: {dataPersil.sub_zona}</li>
+              <li>Luas Bidang: -</li>
+              <li>KDB: {dataPersil.kdb}</li>
+              <li>KLB: {dataPersil.klb}</li>
+              <li>KDH: {dataPersil.kdh}</li>
+              <li>KTB: {dataPersil.ktb}</li>
+              <li>Ketinggian: {dataPersil.tinggi_m_dummy}</li>
+              <li>PSL: {dataPersil.psl}</li>
+              <li>Tipe: {dataPersil.tipe}</li>
+            </ul>
+          ) : (
+            <h2 className="font-poppins text-xs text-blue-400 text-center">
+              Anda Belum Memilih Lokasi Untuk Pengajuan IMB
+            </h2>
+          )}
         </div>
         <div className="flex justify-evenly items-center my-3 font-poppins text-xs">
           <button className="border-2 rounded-lg p-2 w-28">Batal</button>
@@ -21,6 +57,10 @@ function SearchPersil() {
           </button>
         </div>
       </div>
+      <PersilMap
+        className="w-3/4 h-[calc(100vh_-_9.5rem)] p-4 flex flex-col"
+        {...{ setDataPersil }}
+      />
     </div>
   );
 }
