2820bfc5ed819fccc202ed93823846e18d8f6b2b
pratamamp <pratamamp@gmail.com>
2022-09-12 10:35:11 +0700

    update upload

--- src/pages/upload.jsx
+++ src/pages/upload.jsx
@@ -1,44 +1,74 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { FaGlobe, FaPencilAlt, FaCloudUploadAlt } from "react-icons/fa";
 import { BiTargetLock } from "react-icons/bi";
 import { BsFileEarmarkText } from "react-icons/bs";
 import { Player } from "@lottiefiles/react-lottie-player";
+import axios from "axios";
 
 function UploadFiles() {
-  const [showLoading, setShowLoading] = useState(false);
+  const [showLoading, setShowLoading] = useState(true);
   const [loadingFinished, setFinishedLoading] = useState(false);
+  const inputRef = useRef(null);
+  const [selectedFile, setSelectedFile] = useState();
+
+  function handleUpload(event) {
+    inputRef.current.click();
+  }
+
+  async function handleFileChange(event) {
+    const fileObj = event.target.files && event.target.files[0];
+    if (!fileObj) {
+      return;
+    }
+
+    const formData = new FormData();
+
+    formData.append("selectedFile", selectedFile);
+    try {
+      const response = await axios({
+        method: "post",
+        url: "/api/upload/file",
+        data: formData,
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+    // reset file input
+    event.target.value = null;
+  }
 
   return (
     <div className="w-full h-[calc(100vh_-_9.5rem)] flex justify-center items-center">
       <div className="w-9/12 h-5/6 bg-white rounded-lg shadow-lg shadow-gray-300 font-poppins flex flex-col">
         <div className="flex flex-grow">
-          <div className="w-2/5 px-10 py-5">
+          <div className="w-2/5 m-16">
             <h2 className="font-semibold text-xl">Upload GPA 3D</h2>
-            <p className=" text-[#5C5C5C] my-3">
+            <p className=" text-[#5C5C5C] my-5">
               Sebelum mengupload file . harap perhatikanlah ketentuan file
               dibawah ini
             </p>
             <div className="flex flex-col divide-y mt-10 space-y-2">
-              <div className="flex items-center h-10 space-x-3 ">
+              <div className="flex items-center space-x-3 py-3">
                 <FaGlobe className="w-6 h-6 text-[#12519E]" />
                 <h2 className="text-[#717171]">
                   Sudah dilakukan geo-referensi
                 </h2>
               </div>
-              <div className="items-center flex h-10 space-x-3">
+              <div className="items-center flex space-x-3 py-3">
                 <BiTargetLock className="w-6 h-6 text-[#12519E]" />
                 <h2 className="text-[#717171]">
                   Menggunakan sistem koordinat wgs 1984
                 </h2>
               </div>
-              <div className="flex items-center h-10 space-x-3">
-                <BsFileEarmarkText className="w-6 h-6 text-[#12519E]" />
+              <div className="flex items-center space-x-3 py-3">
+                <BsFileEarmarkText className="w-7 h-7 text-[#12519E]" />
                 <h2 className="text-[#717171]">
                   Unggah model 3d GPA berbasis BIM dalam format .SKP atau .RVT
                 </h2>
               </div>
-              <div className="flex items-center h-10 space-x-3">
-                <FaPencilAlt className="w-4 h-4 text-[#12519E]" />
+              <div className="flex items-center space-x-3 py-3">
+                <FaPencilAlt className="w-6 h-6 text-[#12519E]" />
                 <h2 className="text-[#717171]">Ketentuan lainnya</h2>
               </div>
             </div>
@@ -46,8 +76,8 @@ function UploadFiles() {
 
           {/* dropbox */}
 
-          <div className="flex-auto flex pt-5">
-            <div className="rounded-md bg-white/40 w-11/12 border border-sky-400 flex flex-col justify-center items-center">
+          <div className="flex-auto flex m-16">
+            <div className="rounded-md bg-white/40 w-full border border-[#0161D5] flex flex-col justify-center items-center">
               {showLoading ? (
                 <div>
                   <Player
@@ -64,14 +94,25 @@ function UploadFiles() {
                     <p className="text-[#37474F]">
                       <span className="font-bold">
                         Taruh file .SKP atau .RVT disini
-                      </span>
+                      </span>{" "}
                       atau klik untuk menggunggah
                     </p>
-                    <p className="text-[#717171]">Maximum file size 100 MB</p>
+                    <p className="text-[#717171] text-center">
+                      Maximum file size 100 MB
+                    </p>
                   </div>
-                  <button className="rounded-lg border-[#BDBDBD] border p-2 font-poppins text-xs text-[#12519E] font-semibold">
+                  <button
+                    onClick={handleUpload}
+                    className="rounded-xl mt-12 border-[#BDBDBD] border p-2 px-5 font-poppins text-[#12519E] font-semibold"
+                  >
                     Upload +
                   </button>
+                  <input
+                    type="file"
+                    ref={inputRef}
+                    onChange={handleFileChange}
+                    className="hidden"
+                  />
                 </>
               )}
             </div>
@@ -80,11 +121,15 @@ function UploadFiles() {
           {/* end dropbox */}
         </div>
 
-        <div className="w-full flex items-center mt-auto w-full p-5 justify-end space-x-4 px-12 font-poppins font-semibold text-xs">
+        <div className="w-full flex items-center mt-auto w-full justify-end space-x-4 font-poppins font-semibold text-xs px-16 py-4">
           <button className="border border-[#BDBDBD] rounded-lg py-2 w-28 text-[#757575]">
             Batal
           </button>
-          <button className="rounded-lg py-2 w-28 bg-[#EEEEEE] text-[#757575] border border-[#EEEEEE]">
+          <button
+            className={`rounded-lg py-2 w-28 text-[#757575] border border-[#EEEEEE] bg-[#EEEEEE] ${
+              selectedFile ? "bg-blue-300" : ""
+            }`}
+          >
             Selanjutnya
           </button>
         </div>
@@ -94,58 +139,3 @@ function UploadFiles() {
 }
 
 export default UploadFiles;
-
-{
-  /* <div className="flex">
-          <div className="m-10 mr-28 w-1/4 font-poppins">
-            <h2 className="font-semibold text-lg">Upload GPA 3D</h2>
-            <p className="text-xs text-[#5C5C5C] my-3">
-              Sebelum mengupload file . harap perhatikanlah ketentuan file
-              dibawah ini
-            </p>
-            <div className="flex flex-col divide-y text-xs mt-10">
-              <div className="flex items-center h-10 space-x-3">
-                <FaGlobe className="w-4 h-4 text-[#12519E]" />
-                <h2 className="text-[#717171]">
-                  Sudah dilakukan geo-referensi
-                </h2>
-              </div>
-              <div className="items-center flex h-10 space-x-3">
-                <BiTargetLock className="w-4 h-4 text-[#12519E]" />
-                <h2 className="text-[#717171]">
-                  Menggunakan sistem koordinat wgs 1984
-                </h2>
-              </div>
-              <div className="flex items-center h-10 space-x-3">
-                <BsFileEarmarkText className="w-4 h-4 text-[#12519E]" />
-                <h2 className="text-[#717171]">
-                  Unggah model 3d GPA berbasis BIM dalam format .SKP atau .RVT
-                </h2>
-              </div>
-              <div className="flex items-center h-10 space-x-3">
-                <FaPencilAlt className="w-4 h-4 text-[#12519E]" />
-                <h2 className="text-[#717171]">Ketentuan lainnya</h2>
-              </div>
-            </div>
-          </div>
-
-          <div className=" border rounded-md border-[#0161D5] flex flex-col justify-center bg-[#FAFCFE] w-80 items-center">
-            <ImCloudUpload className="w-20 h-20 mb-5 text-[#0161D5]" />
-            <p className="font-poppins text-xs text-[#37474F]">
-              <span className="font-bold">
-                Taruh file .SKP atau .RVT disini
-              </span>{" "}
-              atau klik untuk menggunggah
-            </p>
-            <p className="font-poppins text-xs text-[#717171]">
-              Maximum file size 100 MB
-            </p>
-            <button className="mt-20 rounded-lg border-[#BDBDBD] border p-2 font-poppins text-xs text-[#12519E] font-semibold">
-              Upload +
-            </button>
-          </div>
-        </div>
-        <div className="flex-auto bg-red-300">
-          <button>Selanjutnya</button>
-        </div> */
-}
