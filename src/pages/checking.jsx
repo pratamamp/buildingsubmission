532ee549f79c21806d20be82c7b4dcab9193eeb1
pratamamp <pratamamp@gmail.com>
2023-02-20 16:06:03 +0700

    update checkpage

--- src/pages/checking.jsx
+++ src/pages/checking.jsx
@@ -11,10 +11,17 @@ import { IoMdClose } from "react-icons/io";
 function CheckingGPA() {
   const navigate = useNavigate();
   const [sharePublic, setSharePublic] = useState("shareoff");
+  const [showPopup, setPopUp] = useState(false);
 
   function handleChange(e) {
     setSharePublic(e.target.value);
   }
+
+  function onShareSubmit(e) {
+    e.preventDefault();
+    console.log(sharePublic);
+  }
+
   const mapRef = useRef();
   /*
   // const buildingLayer = new BuildingSceneLayer({
@@ -83,17 +90,18 @@ function CheckingGPA() {
             Jika tidak, Anda dapat menekan tombol Close untuk menutup Aplikasi.
           </p>
         </div>
-        {/* <div className="flex justify-around items-center font-poppins w-full h-16 mt-auto my-3">
+        <div className="flex justify-around items-center font-poppins w-full h-16 mt-auto my-3">
           <button className="border border-[#757575] text-[#757575] rounded-lg bg-white h-4/5 w-1/3">
             Tutup
           </button>
           <button
             className="border rounded-lg bg-[#12519E] text-white h-4/5 w-1/3"
-            onClick={() => navigate("/")}
+            onClick={() => setPopUp(true)}
+            disabled={showPopup}
           >
-            Save Project
+            Simpan Proyek
           </button>
-        </div> */}
+        </div>
       </div>
       <div className="w-3/4 bg-gray-100 relative">
         <div className="w-full h-[calc(100vh_-_9.5rem)]">
@@ -101,54 +109,63 @@ function CheckingGPA() {
           {/* MAPVIEW */}
         </div>
 
-        <div className="bg-white rounded-md absolute top-1/3 left-10">
-          <div className="flex justify-between items-center p-3">
-            <div className="flex space-x-2">
-              <FiShare2 className="w-6 h-auto text-[#0748A3]" />
-              <h2 className="font-poppins font-semibold">Bagikan</h2>
+        {showPopup && (
+          <div className="bg-white rounded-md absolute top-1/3 left-10 shadow-lg drop-shadow-md">
+            <div className="flex justify-between items-center p-3">
+              <div className="flex space-x-2">
+                <FiShare2 className="w-6 h-auto text-[#0748A3]" />
+                <h2 className="font-poppins font-semibold">Bagikan</h2>
+              </div>
+              <IoMdClose className="w-6 h-6 text-[#9E9E9E]" />
             </div>
-            <IoMdClose className="w-6 h-6 text-[#9E9E9E]" />
+            <form onSubmit={onShareSubmit} method="POST">
+              <div className="border-y-2 p-3 space-y-2">
+                <h2 className="font-poppins">
+                  Apakah anda ingin membagi data yang telah diunggah ke publik ?
+                </h2>
+
+                <div className="space-x-2">
+                  <input
+                    type="radio"
+                    value={"shareon"}
+                    id="shareyes"
+                    checked={sharePublic === "shareon"}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="shareyes">Ya, bagikan ke publik</label>
+                </div>
+                <div className="space-x-2">
+                  <input
+                    type="radio"
+                    value={"shareoff"}
+                    id="shareno"
+                    checked={sharePublic === "shareoff"}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="shareyes">
+                    Tidak, buat data saya private
+                  </label>
+                </div>
+              </div>
+
+              <div className="w-full flex items-center justify-end space-x-4 font-poppins font-semibold text-xs py-3 px-2">
+                <button
+                  className="border border-[#BDBDBD] rounded-lg py-2 w-28 text-[#757575]"
+                  type="button"
+                  onClick={() => setPopUp(false)}
+                >
+                  Tutup
+                </button>
+                <button
+                  type="submit"
+                  className={`border rounded-lg bg-[#12519E] text-white py-2 w-28 `}
+                >
+                  Simpan Proyek
+                </button>
+              </div>
+            </form>
           </div>
-          <form>
-            <div className="border-y-2 p-3 space-y-2">
-              <h2 className="font-poppins">
-                Apakah anda ingin membagi data yang telah diunggah ke publik ?{" "}
-              </h2>
-
-              <div className="space-x-2">
-                <input
-                  type="radio"
-                  value={"shareon"}
-                  id="shareyes"
-                  checked={sharePublic === "shareon"}
-                  onChange={handleChange}
-                />
-                <label htmlFor="shareyes">Ya, bagikan ke publik</label>
-              </div>
-              <div className="space-x-2">
-                <input
-                  type="radio"
-                  value={"shareoff"}
-                  id="shareno"
-                  checked={sharePublic === "shareoff"}
-                  onChange={handleChange}
-                />
-                <label htmlFor="shareyes">Tidak, buat data saya private</label>
-              </div>
-            </div>
-
-            <div className="w-full flex items-center justify-end space-x-4 font-poppins font-semibold text-xs py-3 px-2">
-              <button className="border border-[#BDBDBD] rounded-lg py-2 w-28 text-[#757575]">
-                Tutup
-              </button>
-              <button
-                className={`border rounded-lg bg-[#12519E] text-white py-2 w-28 `}
-              >
-                Simpan Proyek
-              </button>
-            </div>
-          </form>
-        </div>
+        )}
       </div>
     </div>
   );
